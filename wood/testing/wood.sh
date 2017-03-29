22790459d6b0ab3d4cca4eb84a7b0245e017e79d
Jeremie Francois (on alpha) <jeremie.francois@gmail.com>
2017-03-29 22:57:08 +0200

    Fixed useless content due to the copy from colormix

--- wood/testing/wood.sh
+++ wood/testing/wood.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
-killall gnuplot > /dev/null 2>&1
 set -e
 
-input="wood_cylinder_source.gcode"
-random=${1-2}
-speed=${2-100}
-mixCount=3
-
+input=${1-"wood_cylinder_source.gcode"}
 f=$(echo $input| sed 's/_source//')
+
 cp "$input" "$f"
 
-python ../wood.py --file $f
+python ../wood.py --file "$f"
+
